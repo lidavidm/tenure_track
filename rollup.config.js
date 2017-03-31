53489651c12a3259a51891300d9640e40438bc31
David Li <li.davidm96@gmail.com>
2017-03-30 21:48:06 -0400

    chore: enable use of NPM & CommonJS modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
 export default {
@@ -6,6 +7,7 @@ export default {
     format: 'cjs',
     plugins: [
         resolve(),
+        commonjs(),
         babel({
             exclude: 'node_modules/**' // only transpile our source code
         })
