d1b9166e5e506f2d0ddd2206e3c9ae1ab52a9a95
David Li <li.davidm96@gmail.com>
2017-05-22 10:25:28 -0400

    feat(stage/Project): implement topic selection UI

--- src/game/project.js
+++ src/game/project.js
@@ -21,6 +21,30 @@ import { Text, TextButton } from "../ui/text";
 import * as game from "./game";
 import { LabStage } from "./lab";
 
+class SpecializationCard extends BorderBox {
+    constructor(specialization, level, handler=null) {
+        super(new LinearBox([
+            new Text(specialization, { size: 18 }),
+            new Text(`Level ${level}`, { size: 16 }),
+        ], "vertical"));
+        this.handler = handler;
+    }
+
+    onclick() {
+        if (this.handler) {
+            this.handler(this);
+        }
+    }
+
+    highlight() {
+        this.fillColor = "#00F";
+    }
+
+    unhighlight() {
+        this.fillColor = null;
+    }
+}
+
 export class ProjectStartStage extends Stage {
     constructor(state) {
         super();
@@ -40,6 +64,34 @@ export class ProjectStartStage extends Stage {
             this.container.transitionFromTo(this, lab, "above");
         });
         container.add(goback);
+
+        container.add(new Text("Choose a base topic:"));
+
+        let makeHandler = (group) => {
+            return (card) => {
+                group.children.forEach((c) => c.unhighlight());
+                card.highlight();
+            };
+        };
+
+        let specializations = new LinearBox([], "horizontal");
+        let specializationsHandler = makeHandler(specializations);
+        let cards = Object.entries(this.state.professor.specializations).map(([specialization, level]) => {
+            return new SpecializationCard(specialization, level, specializationsHandler);
+        });
+        specializations.addAll(cards);
+        container.add(specializations);
+
+        container.add(new Text("[Optional] Choose a second topic:"));
+        let subtopics = new LinearBox([], "horizontal");
+        let subtopicsHandler = makeHandler(subtopics);
+        let cards2 = game.Project.PRIMARY
+            .filter((s) => !this.state.professor.specializations[s])
+            .map((specialization) => {
+                return new SpecializationCard(specialization, 0, subtopicsHandler);
+            });
+        subtopics.addAll(cards2);
+        container.add(subtopics);
 
         this.add(container);
     }
